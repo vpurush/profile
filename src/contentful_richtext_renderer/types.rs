9ee99a15a47c004757d5ca2207cd5ad7964361c4
Vijay Purush <vijayaraghavan1987@gmail.com>
2024-09-01 22:34:32 +1000

    contentful rich text types

--- src/contentful_richtext_renderer/types.rs
+++ src/contentful_richtext_renderer/types.rs
@@ -1,11 +1,35 @@
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ContentfulRichTextDocument {
-    
+#[serde(tag = "nodeType")]
+pub enum ContentfulRichTextNode {
+    #[serde(rename(serialize = "document", deserialize = "document"))]
+    Document(ContentfulRichTextDocument),
+    #[serde(rename(serialize = "paragraph", deserialize = "paragraph"))]
+    Paragraph(ContentfulRichTextParagraph),
+    #[serde(rename(serialize = "text", deserialize = "text"))]
+    Text(ContentfulRichTextText)
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "nodeType")]
+pub struct ContentfulRichTextDocument {
+    content: Vec<ContentfulRichTextNode>
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "nodeType")]
+pub struct ContentfulRichTextParagraph {
+    content: Vec<ContentfulRichTextNode>
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "nodeType")]
+pub struct ContentfulRichTextText {
+    value: String
+}
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ContentfulRichTextContent {
     json: ContentfulRichTextDocument,
