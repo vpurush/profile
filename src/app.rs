e93cce7de1fa4cda22e4aed13053ea1965aa3d6f
Vijay Purush <vijayaraghavan1987@gmail.com>
2024-09-17 20:41:17 +1000

    playfair font added

--- src/app.rs
+++ src/app.rs
@@ -10,6 +10,9 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
 
         
         // injects a stylesheet into the document <head>
